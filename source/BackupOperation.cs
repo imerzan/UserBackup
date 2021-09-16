79edd1524994e81257182db831a8dde46e178db4
Steve Tolzmann <steven.tolzmann@gmail.com>
2021-09-16 09:20:23 -0500

    Addtl Optimizations

--- source/BackupOperation.cs
+++ source/BackupOperation.cs
@@ -11,22 +11,22 @@ namespace UserBackup
 {
     public class BackupOperation
     {
-        private string[] _users;
-        private string _dest;
-        private bool _scanCompleted;
+        private DirectoryInfo _dest;
+        private readonly List<DirectoryInfo> _users;
         private readonly OSPlatform _platform;
         private readonly BackupCounters _counters;
         private readonly ConcurrentQueue<BackupFile> _queue;
         private readonly BackupLogger _logger;
         private readonly BackupWorker[] _workers;
         private readonly System.Timers.Timer _timer;
-        private readonly EnumerationOptions _enumOptions = new EnumerationOptions()
+        private bool _scanCompleted;
+        private static readonly EnumerationOptions _enumOptions = new EnumerationOptions()
         {
             AttributesToSkip = FileAttributes.Hidden | FileAttributes.System,
             IgnoreInaccessible = true,
             ReturnSpecialDirectories = false
         };
-        private readonly List<string> _ExcludedDirectories = new List<string>() // Excluded Directories in root %UserProfile%
+        private static readonly List<string> _ExcludedDirectories = new List<string>() // Excluded Directories in root %UserProfile%
         {
             "Library",
             "Applications",
@@ -40,8 +40,9 @@ namespace UserBackup
 
         public BackupOperation(OSPlatform platform, string dest, int threads)
         {
+            if (dest is not null) _dest = new DirectoryInfo(dest.TrimEnd(Path.DirectorySeparatorChar));
             _platform = platform;
-            _dest = dest;
+            _users = new List<DirectoryInfo>();
             _scanCompleted = false;
             _counters = new BackupCounters();
             _queue = new ConcurrentQueue<BackupFile>();
@@ -129,12 +130,12 @@ namespace UserBackup
             Console.Write("Enter User(s) by id# to backup>> ");
             string users = Console.ReadLine().Trim();
             if (users == String.Empty) throw new Exception("User id(s) cannot be blank!");
-            _users = users.Split(',');
-            for (int i = 0; i < _users.Length; i++)
+            foreach (var x in users.Split(','))
             {
-                if (!int.TryParse(_users[i].Trim(), out int id)) throw new Exception("Invalid user id number(s) entered!");
-                _users[i] = userDirs[id];
-                if (!Directory.Exists(_users[i])) throw new IOException($"User Path {_users[i]} does not exist!");
+                if (!int.TryParse(x.Trim(), out int id)) throw new Exception("Invalid user id number(s) entered!");
+                var user = new DirectoryInfo(userDirs[id]);
+                if (!user.Exists) throw new IOException($"User Path {user.FullName} does not exist!");
+                _users.Add(user);
             }
         }
 
@@ -144,12 +145,12 @@ namespace UserBackup
             {
                 Console.WriteLine($"\nDEFAULT Destination Path: {Path.Combine(Directory.GetCurrentDirectory(), "Backups")}");
                 Console.Write("\nEnter Dest Path (blank=default)>> ");
-                _dest = Console.ReadLine().Trim().TrimEnd(Path.DirectorySeparatorChar);
-                if (_dest == String.Empty) _dest = Path.Combine(Directory.GetCurrentDirectory(), "Backups");
+                string dest = Console.ReadLine().Trim().TrimEnd(Path.DirectorySeparatorChar);
+                if (dest == String.Empty) _dest = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "Backups"));
+                else _dest = new DirectoryInfo(dest);
             }
-            _dest = Path.GetFullPath(_dest.TrimEnd(Path.DirectorySeparatorChar));
-            var create = Directory.CreateDirectory(_dest);
-            if (!create.Exists) throw new IOException($"Unable to create destination folder {_dest}");
+            _dest.Create(); // Attempt to create dest folder
+            if (!_dest.Exists) throw new IOException($"Unable to create destination folder {_dest}");
         }
 
         private void PromptName()
@@ -157,19 +158,18 @@ namespace UserBackup
             Console.Write($"\nEnter Backup Name (default:{Environment.MachineName})>> ");
             string backupName = Console.ReadLine().Trim().Replace(Path.DirectorySeparatorChar.ToString(), String.Empty);
             if (backupName == String.Empty) backupName = Environment.MachineName;
-            _dest = Path.Combine(_dest, backupName);
-            if (Directory.Exists(_dest))
+            if (Directory.Exists(Path.Combine(_dest.FullName, backupName)))
             {
-                _dest += $"_{Path.GetRandomFileName()}"; // Rename trailing directory name
+                _dest = _dest.CreateSubdirectory($"{backupName}_{Path.GetRandomFileName()}"); // Rename trailing directory name
             }
-            var create = Directory.CreateDirectory(_dest); // Create backup sub-folder
-            if (!create.Exists) throw new IOException($"Unable to create destination folder {_dest}");
+            else _dest = _dest.CreateSubdirectory(backupName);
+            if (!_dest.Exists) throw new IOException($"Unable to create destination folder {_dest}");
         }
 
         private void BackupProfile()
         {
-            _logger.Open(Path.Combine(_dest, "log.txt")); // Open Logfile
-            var sw = new BackupStopwatch(_logger, _counters, _dest); // Stopwatch to track backup duration
+            _logger.Open(Path.Combine(_dest.FullName, "log.txt")); // Open Logfile
+            var sw = new BackupStopwatch(_logger, _counters, _dest.FullName); // Stopwatch to track backup duration
             for (int i = 0; i < _workers.Length; i++) // Start Workers
             {
                 _workers[i] = new BackupWorker(_counters, _queue, _logger, i);
@@ -177,16 +177,15 @@ namespace UserBackup
             _timer.Start(); // Start timer for progress updates
             foreach (var user in _users) // Iterate *all* selected users
             {
-                var userName = Path.GetFileName(user);
-                _logger.Submit($"** Scanning User '{userName}'");
+                _logger.Submit($"** Scanning User '{user.Name}'");
                 if (_platform == OSPlatform.OSX) // Mac Only
                 {
                     try // Safari bookmarks
                     {
-                        var safari = new FileInfo(Path.Combine(user, "Library/Safari/Bookmarks.plist"));
+                        var safari = new FileInfo(Path.Combine(user.FullName, "Library/Safari/Bookmarks.plist"));
                         if (safari.Exists)
                         {
-                            var dest = Directory.CreateDirectory(Path.Combine(_dest, userName, "SafariBookmarks"));
+                            var dest = Directory.CreateDirectory(Path.Combine(_dest.FullName, user.Name, "SafariBookmarks"));
                             _queue.Enqueue(new BackupFile()
                             {
                                 Source = safari.FullName,
@@ -205,10 +204,10 @@ namespace UserBackup
                 {
                     try // Microsoft Edge Bookmarks (Chromium Version)
                     {
-                        var edge = new FileInfo(Path.Combine(user, @"AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks"));
+                        var edge = new FileInfo(Path.Combine(user.FullName, @"AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks"));
                         if (edge.Exists)
                         {
-                            var dest = Directory.CreateDirectory(Path.Combine(_dest, userName, "EdgeBookmarks"));
+                            var dest = Directory.CreateDirectory(Path.Combine(_dest.FullName, user.Name, "EdgeBookmarks"));
                             _queue.Enqueue(new BackupFile()
                             {
                                 Source = edge.FullName,
@@ -227,10 +226,10 @@ namespace UserBackup
                 {
                     if (_platform == OSPlatform.OSX)
                     {
-                        var chrome = new FileInfo(Path.Combine(user, "Library/Application Support/Google/Chrome/Default/Bookmarks"));
+                        var chrome = new FileInfo(Path.Combine(user.FullName, "Library/Application Support/Google/Chrome/Default/Bookmarks"));
                         if (chrome.Exists)
                         {
-                            var dest = Directory.CreateDirectory(Path.Combine(_dest, userName, "ChromeBookmarks"));
+                            var dest = Directory.CreateDirectory(Path.Combine(_dest.FullName, user.Name, "ChromeBookmarks"));
                             _queue.Enqueue(new BackupFile()
                             {
                                 Source = chrome.FullName,
@@ -241,10 +240,10 @@ namespace UserBackup
                     }
                     else if (_platform == OSPlatform.Windows)
                     {
-                        var chrome = new FileInfo(Path.Combine(user, @"AppData\Local\Google\Chrome\User Data\Default\Bookmarks"));
+                        var chrome = new FileInfo(Path.Combine(user.FullName, @"AppData\Local\Google\Chrome\User Data\Default\Bookmarks"));
                         if (chrome.Exists)
                         {
-                            var dest = Directory.CreateDirectory(Path.Combine(_dest, userName, "ChromeBookmarks"));
+                            var dest = Directory.CreateDirectory(Path.Combine(_dest.FullName, user.Name, "ChromeBookmarks"));
                             _queue.Enqueue(new BackupFile()
                             {
                                 Source = chrome.FullName,
@@ -263,19 +262,19 @@ namespace UserBackup
                 {
                     if (_platform == OSPlatform.OSX)
                     {
-                        var firefox = new DirectoryInfo(Path.Combine(user, "Library/Application Support/Firefox/Profiles"));
+                        var firefox = new DirectoryInfo(Path.Combine(user.FullName, "Library/Application Support/Firefox/Profiles"));
                         if (firefox.Exists)
                         {
-                            var dest = Directory.CreateDirectory(Path.Combine(_dest, userName, "FirefoxBookmarks"));
+                            var dest = Directory.CreateDirectory(Path.Combine(_dest.FullName, user.Name, "FirefoxBookmarks"));
                             ProcessDirectory(firefox, dest);
                         }
                     }
                     else if (_platform == OSPlatform.Windows)
                     {
-                        var firefox = new DirectoryInfo(Path.Combine(user, @"AppData\Roaming\Mozilla\Firefox\Profiles"));
+                        var firefox = new DirectoryInfo(Path.Combine(user.FullName, @"AppData\Roaming\Mozilla\Firefox\Profiles"));
                         if (firefox.Exists)
                         {
-                            var dest = Directory.CreateDirectory(Path.Combine(_dest, userName, "FirefoxBookmarks"));
+                            var dest = Directory.CreateDirectory(Path.Combine(_dest.FullName, user.Name, "FirefoxBookmarks"));
                             ProcessDirectory(firefox, dest);
                         }
                     }
@@ -285,7 +284,7 @@ namespace UserBackup
                     _logger.Submit($"ERROR processing Firefox Bookmarks: {ex}");
                     Interlocked.Increment(ref _counters.ErrorCount);
                 }
-                ProcessDirectory(new DirectoryInfo(user), new DirectoryInfo(_dest), true); // Start recursive call
+                ProcessDirectory(user, _dest, true); // Start recursive call
             } // User Backup Completed (ForEach)
             _logger.Submit("** Backup scan completed! Waiting for queue to be completed by Worker Threads...");
             _scanCompleted = true; // Set 'Scan Completed' flag
@@ -306,7 +305,7 @@ namespace UserBackup
 
         private void ProcessDirectory(DirectoryInfo directory, DirectoryInfo backupDest, bool isRoot = false)
         {
-            if (directory.FullName.Contains(_dest, StringComparison.OrdinalIgnoreCase)) // Loop Protection
+            if (directory.FullName.Contains(_dest.FullName, StringComparison.OrdinalIgnoreCase)) // Loop Protection
             {
                 _logger.Submit($"WARNING - Backup loop detected! Skipping folder {directory.FullName}");
                 return;
